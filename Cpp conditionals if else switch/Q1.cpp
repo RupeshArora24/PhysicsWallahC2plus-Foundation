788fcca5ce052cf3493f5963d9a9db449bdd5bb7
Rupesh Arora <102988343+RupeshArora24@users.noreply.github.com>
2023-07-22 10:32:26 +0530

    Update Q1.cpp
    If-elsecondition to check weather the shape is a squaere or not

--- Cpp conditionals if else switch/Q1.cpp
+++ Cpp conditionals if else switch/Q1.cpp
@@ -1,6 +1,7 @@
 /*Q1 - Write a program which takes the values of length and breadth from user and check if it is
 a square or not.*/
 #include<iostream>
+//If-elsecondition to check weather the shape is a squaere or not
 using namespace std;
 int main(){
     int length,bredth;
